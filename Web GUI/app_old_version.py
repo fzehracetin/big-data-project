c96959fd91525e9e4152cadb3d8b2dbb11ad1dc5
Fatma Zehra Çetin <37731704+fzehracetin@users.noreply.github.com>
2021-06-08 12:21:54 +0300

    Hdfs commands added, background changed.

--- Web GUI/app_old_version.py
+++ Web GUI/app_old_version.py
@@ -14,10 +14,10 @@ if not os.path.isdir(DOWNLOAD_FOLDER):
 
 app = Flask(__name__)
 
-client = InsecureClient('localhost:9870')
+client = InsecureClient('http://localhost:9000')
 file_name = ""
 hdfs_input_path = "input"
-hdfs_output_path = "outputTestWeb5"
+hdfs_output_name = "outputTest"
 
 
 @app.route('/')
@@ -31,27 +31,14 @@ def download_from_hdfs(hdfs_path):
     file_name = hdfs_path.split('/')[-1]
 
 
-def upload_to_hdfs(local_path, hdfs_path, selected_function):
-    filename = local_path.split("\\")[-1]
-    print(filename)
-    cmd_command = "docker cp " + local_path + " namenode:/home/datasets/" + filename
-    print(cmd_command)
-    
-    cmd_command2 = "docker exec -it namenode hadoop fs -put /home/datasets/" + filename + " input"
-    print(cmd_command2)
-    
-    #cmd_command3 = "docker exec -it namenode hdfs dfs -put ./input/* input"
-    cmd_command3 = "docker exec -it namenode hadoop jar mapReduce.jar " + selected_function + " input " + \
-                  hdfs_output_path
-    
-    cmd_command4 = "docker exec -it namenode hadoop fs -getmerge " + hdfs_output_path + " home/datasets/" + hdfs_output_path + ".txt"
-    cmd_command5 = "docker cp namenode:home/datasets/" + hdfs_output_path + ".txt downloads\\" + hdfs_output_path + ".txt"
-    
-    os.system('cmd /c "{} & {} & {} & {} & {}"'.format(cmd_command, cmd_command2, cmd_command3, cmd_command4, cmd_command5))
-    
+def upload_to_hdfs(local_path, hdfs_path):
+    client.upload(hdfs_path, local_path)
+    # os.system('cmd /k "{}"'.format(cmd_command))
+    # hdfs dfs - put. / input / * input
+
 
 def run_job(hdfs_output_path, selected_function):  # "Average"
-    cmd_command = "docker exec -it namenode hadoop jar mapReduce.jar " + selected_function + " input " + \
+    cmd_command = "docker exec -it namenode hadoop jar MapReduce.jar " + selected_function + " input " + \
                   hdfs_output_path
     os.system('cmd /k "{}"'.format(cmd_command))
     return
@@ -68,13 +55,11 @@ def upload_file():
     print("File: {}, Function: {}".format(uploaded_file.filename, selected_function))
 
     # upload file to hdfs
-    full_path = "C:\\Users\\fazb7\\Desktop\\SchoolProjects\\BigData\\Web_GUI\\WebGUI\\uploads\\" + uploaded_file.filename
-    print(full_path)
-    upload_to_hdfs(full_path, hdfs_input_path, selected_function)
-    #UPLOAD_FOLDER + "\\" + secure_filename(uploaded_file.filename),
+    upload_to_hdfs(UPLOAD_FOLDER + "/" + secure_filename(uploaded_file.filename), hdfs_input_path)
+
     # run jobs here
-    #run_job(hdfs_output_path, selected_function)
-    
+    run_job(hdfs_output_name, selected_function)
+
     end = time.time()
     time_elapsed = end - start
     return render_template('main.html', run_display="none", result_display="block", function=selected_function,
@@ -86,9 +71,12 @@ def result():
     if request.form['Submit'] == 'Go Back':
         return render_template('main.html', run_display="block", result_display="none")
     elif request.form['Submit'] == 'Download the Result':
-        #download_from_hdfs(hdfs_output_path)
-        #return render_template('main.html', run_display="none", result_display="block")
-        return send_file(DOWNLOAD_FOLDER + "\\" + hdfs_output_path + ".txt", mimetype='text/plain', attachment_filename=file_name, as_attachment=True)
+        download_from_hdfs(hdfs_output_name)
+        return render_template('main.html', run_display="none", result_display="block")
+        # return send_file(DOWNLOAD_FOLDER + "/" + file_name,
+        #                  mimetype='text/plain',
+        #                  attachment_filename=file_name,
+        #                  as_attachment=True)
 
 
 if __name__ == "__main__":
